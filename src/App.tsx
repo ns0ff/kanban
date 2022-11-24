8cb49bf90df0222f6c7285db9faa37e44ae62091
Nick <hellons0ff@gmail.com>
2022-11-24 20:24:07 +0300

    1. basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
+import { List } from './List';
+
+const data = [
+    {
+        id: 1,
+        title: 'What to learn',
+        tasks: [{id: 1, title: 'CSS', isDone: false}, {id: 2, title: 'JS', isDone: false}, {id: 3, title: 'React', isDone: true}, {id: 3, title: 'HTML', isDone: true}]
+},
+    {
+        id: 2,
+        title: 'What to buy',
+        tasks: [{id: 1, title: 'Beer', isDone: false}, {id: 2, title: 'Water', isDone: false}, {id: 3, title: 'Bread', isDone: true}, {id: 3, title: 'Laptop', isDone: true}]
+},
+    
+]
 
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <List id={data[0].id} title={data[0].title} tasks={data[0].tasks}/>
+            <List id={data[1].id} title={data[1].title} tasks={data[1].tasks}/>
         </div>
     );
 }
---
+++ src/List.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type ListType = {
+    id: number
+    title: string
+    tasks: Array<TaskType>
+}
+
+export const List = (props: ListType) => {
+    return (
+        <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li key={props.id}><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                    <li key={props.id}><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                    <li key={props.id}><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+                    <li key={props.id}><input type="checkbox" checked={props.tasks[3].isDone}/> <span>{props.tasks[3].title}</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+    )
+}
