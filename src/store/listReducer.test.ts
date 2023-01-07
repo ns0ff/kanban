bd6d35a04e591598ad0af12425768bf685054f8e
Nick <74413828+ns0ff@users.noreply.github.com>
2023-01-07 17:13:49 +0300

    list reducer, action creators, reducer.test completed

--- src/store/listReducer.test.ts
+++ src/store/listReducer.test.ts
@@ -1,6 +1,6 @@
 import { v1 } from "uuid"
 import { TodoListsType } from "../App"
-import { REMOVE_LIST, listsReducer } from "./listReducer"
+import { RemoveListAC, listsReducer, AddListAC, RenameListAC, ChangeListFilterAC } from "./listReducer"
 
 test ('correct list should be removed', () => {
     const listID1 = v1()
@@ -11,8 +11,56 @@ test ('correct list should be removed', () => {
         {id: listID2, title: 'Gear cheklist', filter: 'all'}
     ]
 
-    const finalState = listsReducer(initialState, {type: REMOVE_LIST, id: listID1})
+    const finalState = listsReducer(initialState, RemoveListAC(listID1))
 
     expect(finalState.length).toBe(1)
     expect(finalState[0].id).toBe(listID2)
+})
+
+test ('correct list should be added', () => {
+    const listID1 = v1()
+    const listID2 = v1()
+    const newListTitle = 'Another List'
+
+    const initialState: Array<TodoListsType> = [
+        {id: listID1, title: 'Camera setup checklist', filter: 'all'},
+        {id: listID2, title: 'Gear cheklist', filter: 'all'}
+    ]
+
+    const finalState = listsReducer(initialState, AddListAC(newListTitle))
+
+    expect(finalState.length).toBe(3)
+    expect(finalState[2].title).toBe(newListTitle)
+})
+
+test ('list shoul be renamed', () => {
+    const listID1 = v1()
+    const listID2 = v1()
+    const newListTitle = 'Another List'
+
+    const initialState: Array<TodoListsType> = [
+        {id: listID1, title: 'Camera setup checklist', filter: 'all'},
+        {id: listID2, title: 'Gear cheklist', filter: 'all'}
+    ]
+
+    const finalState = listsReducer(initialState, RenameListAC(newListTitle, listID1))
+
+    expect(finalState.length).toBe(2)
+    expect(finalState[0].title).toBe(newListTitle)
+})
+
+test ('filter should be changed', () => {
+    const listID1 = v1()
+    const listID2 = v1()
+    const newListFilter = 'completed'
+
+    const initialState: Array<TodoListsType> = [
+        {id: listID1, title: 'Camera setup checklist', filter: 'all'},
+        {id: listID2, title: 'Gear cheklist', filter: 'all'}
+    ]
+
+    const finalState = listsReducer(initialState, ChangeListFilterAC(newListFilter, listID1))
+
+    expect(finalState.length).toBe(2)
+    expect(finalState[0].filter).toBe(newListFilter)
 })
--- src/store/listReducer.ts
+++ src/store/listReducer.ts
@@ -1,17 +1,52 @@
-import { TodoListsType } from "../App";
+import { v1 } from "uuid";
+import { FilterType, TodoListsType } from "../App";
 
 export const REMOVE_LIST = 'REMOVE-LIST' as const
+export const ADD_LIST = 'ADD-LIST' as const
+export const RENAME_LIST = 'RENAME-LIST' as const
+export const CHANGE_LIST_FILTER = 'CHANGE-LIST-FILTER' as const
 
-type RemoveListAC = {
+type ActionsType = RemoveListAT | AddListAT | RenameListAT | ChangeListFilterAT
+type RemoveListAT = {
     type: typeof REMOVE_LIST
     id: string
 }
+type AddListAT = {
+    type: typeof ADD_LIST
+    title: string
+}
+type RenameListAT = {
+    type: typeof RENAME_LIST
+    id: string
+    title: string
+}
+type ChangeListFilterAT = {
+    type: typeof CHANGE_LIST_FILTER
+    id: string
+    filter: FilterType
+}
 
-export const listsReducer = (todolists: Array<TodoListsType>, action: any): Array<TodoListsType> => {
+export const listsReducer = (lists: Array<TodoListsType>, action: ActionsType): Array<TodoListsType> => {
     switch (action.type) {
         case 'REMOVE-LIST' : 
-                return todolists.filter(list => list.id !== action.id)
+                return lists.filter(list => list.id !== action.id)
+        case 'ADD-LIST' : 
+                 const newList: TodoListsType = {
+                    id: v1(),
+                    title: action.title,
+                    filter: "all"
+                  }
+                  return [...lists, newList]
+        case 'RENAME-LIST' : 
+                 return lists.map(list => list.id === action.id ? {...list, title: action.title } : list)
+        case 'CHANGE-LIST-FILTER' : 
+                 return lists.map(list => list.id === action.id ? {...list, filter: action.filter } : list)
         default: 
-            return todolists
+            return lists
     }
 }
+
+export const RemoveListAC = (id: string): RemoveListAT => ({type: REMOVE_LIST, id })
+export const AddListAC = (title: string): AddListAT => ({type: ADD_LIST, title})
+export const RenameListAC = (title: string, id: string): RenameListAT => ({type: RENAME_LIST, id, title})
+export const ChangeListFilterAC = (filter: FilterType, id: string): ChangeListFilterAT => ({type: CHANGE_LIST_FILTER, filter, id})
